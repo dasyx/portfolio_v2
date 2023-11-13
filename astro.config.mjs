de7e0dd6e5c68f81129ee2bce3422b3d13f55044
Levast David <dasyx@MacBook-Pro-de-Levast.local>
2023-11-13 22:09:21 +0100

    update vercel in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from '@astrojs/vercel/edge';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind()],
   output: "server",
-  adapter: vercel({
-    imageService: true,
-  }),
+  adapter: vercel(),
 });
