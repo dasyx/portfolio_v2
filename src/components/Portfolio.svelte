f0cb1b0177da6290083000e40d5fa3bc0fcc2007
unknown <seanpnn00@gmail.com>
2024-01-17 00:27:46 +0100

    portfolio section responsive fix

--- src/components/Portfolio.svelte
+++ src/components/Portfolio.svelte
@@ -7,6 +7,12 @@
       projectUrl: "https://groupo-frontend.vercel.app/",
       codeUrl: "https://github.com/dasyx/groupo_backend",
       technologies: ["VUE", "SASS", "BULMA", "NODE", "MYSQL"],
+      mustsees: [
+        "Création d'un réseau social d'entreprise",
+        "Serveur Node.js + express + JWT & Bcrypt",
+        "API RESTful",
+        "Base de données MySQL",
+      ],
     },
     {
       imgSrc: "/projects-img/mlindustrie.webp",
@@ -14,6 +20,12 @@
       projectUrl: "https://mlindustrie.fr",
       codeUrl: "https://github.com/dasyx/mlindustrie_local",
       technologies: ["VUE", "SASS", "NODE", "MONGODB"],
+      mustsees: [
+        "Site web sécurisé (OWASP friendly)",
+        "Espace utilisateur (RGPD friendly) full access",
+        "Base de données MongoDB",
+        "Backend sur Heroku + Frontend sur Netlify",
+      ],
     },
     {
       imgSrc: "/projects-img/pasquier.webp",
@@ -21,6 +33,11 @@
       projectUrl: "https://meublesgerardpasquier.fr/",
       codeUrl: "https://github.com/dasyx/meubles_gerard_pasquier",
       technologies: ["HTML", "CSS", "JS", "PARCEL"],
+      mustsees: [
+        "Site web commercial multi-pages design",
+        "Utilisation de Parcel pour le bundling",
+        "Responsive design",
+      ],
     },
     {
       imgSrc: "/projects-img/scribe.webp",
@@ -28,24 +45,39 @@
       projectUrl: "https://majestic-tarsier-2ac220.netlify.app",
       codeUrl: "https://github.com/dasyx/client_scribe",
       technologies: ["SVELTE", "TAILWIND", "SUPABASE"],
+      mustsees: [
+        "Application web de gestion devis/factures",
+        "Intégration du CRUD de Supabase",
+        "Utilisation de Vite pour le bundling",
+      ],
     },
+    /* {
+      imgSrc: "/projects-img/vocationa.webp",
+      altText: "vocationa",
+      projectUrl: "https://main--stellar-salamander-c473dd.netlify.app/",
+      codeUrl: "https://github.com/dasyx/vocationa",
+      technologies: ["SVELTE", "TAILWIND", "FAUNABD", "CLERK"],
+      mustsees: [
+        "Application web d'immersion professionnelle",
+        "Intégration du CRUD de FaunaDB",
+        "Utilisation de Clerk pour l'authentification",
+      ],
+    }, */
   ];
 </script>
 
 <div
   class="portfolio-container grid grid-cols-4 md:grid-cols-6 sm:grid-cols-12"
 >
-  {#each projets as projet}
+  <h3 class="absolute top-16 left-12">PORTFOLIO</h3>
+  {#each projets as projet, index}
     <div
-      class="col-span-4 sm:col-span-12 mb-36 mt-38 md:col-span-6 mb-42 lg:pl-6 mb-19 mt-39"
+      class="col-span-4 sm:col-span-12 md:col-span-6 lg:pl-6 project-item project-{index}"
     >
-      <div class="flex flex-col md:flex-row">
-        <!-- Modifier ici la classe de hauteur ou ajouter un style personnalisé -->
+      <div class="flex flex-col lg:flex-row">
         <figure
-          class="group relative overflow-hidden blur-on-hover w-full md:w-3/4 h-64
-          lg:w-1/3"
+          class="group relative overflow-hidden blur-on-hover w-full h-64 lg:w-1/3"
         >
-          <!-- Exemple avec h-64 -->
           <img
             src={projet.imgSrc}
             alt={projet.altText}
@@ -55,8 +87,7 @@
             class="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out flex flex-col w-full h-full"
           >
             <div
-              class="flex-1
-bg-gray-800 bg-opacity-60 flex items-center justify-center"
+              class="flex-1 bg-gray-800 bg-opacity-60 flex items-center justify-center"
             >
               <a
                 href={projet.projectUrl}
@@ -77,14 +108,21 @@ bg-gray-800 bg-opacity-60 flex items-center justify-center"
             </div>
           </div>
         </figure>
-        <div class="descriptif-projet mt-2 md:mt-0 md:ml-4 w-full md:w-1/2">
+        <div class="descriptif-projet mt-2 md:mt-0 md:ml-4 w-full">
           {#each projet.technologies as tech}
             <span
               class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
               >{tech}</span
             >
           {/each}
-          <p>{projet.description}</p>
+          <div class="label-et-listes">
+            <span class="label">En savoir plus</span>
+            <ul class="listes-a-puces">
+              {#each projet.mustsees as mustsee}
+                <li>{mustsee}</li>
+              {/each}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
@@ -103,9 +141,38 @@ bg-gray-800 bg-opacity-60 flex items-center justify-center"
     filter: blur(5px); /* Ajustez la valeur du flou selon vos préférences */
   }
   .portfolio-container {
-    margin-top: 12rem;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
   }
   .portfolio-container figure {
     margin-bottom: 1rem;
+  }
+  .project-0 {
+    margin-top: 14rem;
+  }
+  .project-1,
+  .project-2,
+  .project-3 {
+    margin-top: 3rem;
+  }
+  .project-item:last-child {
+    margin-bottom: 0;
+  }
+  .label-et-listes {
+    border-top: 1px solid #ccc;
+    padding-top: 10px;
+  }
+
+  .label-et-listes .label {
+    display: block;
+    text-align: center;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+
+  .label-et-listes .listes-a-puces {
+    list-style-type: disc;
+    padding-left: 20px;
   }
 </style>
