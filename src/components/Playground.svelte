886a7d3a62fb050905433328f1775246121a6461
unknown <seanpnn00@gmail.com>
2024-01-18 00:37:47 +0100

    add two last sections projects

---
+++ src/components/Playground.svelte
@@ -0,0 +1,149 @@
+<script>
+  // On peut utiliser la prop 'projets' dans le script
+  export const projets = [
+    {
+      imgSrc: "/projects-img/vocationa.webp",
+      altText: "vocationa",
+      projectUrl: "https://main--stellar-salamander-c473dd.netlify.app/",
+      codeUrl: "https://github.com/dasyx/vocationa",
+      technologies: ["SVELTE", "TAILWIND", "FAUNABD", "CLERK"],
+      mustsees: [
+        "Application web d'immersion professionnelle",
+        "Intégration du CRUD de FaunaDB",
+        "Utilisation de Clerk pour l'authentification",
+      ],
+    },
+    {
+      imgSrc: "/projects-img/diary.webp",
+      altText: "impersonator diary",
+      projectUrl: "https://main--incandescent-bavarois-f1d9e2.netlify.app/",
+      codeUrl: "https://github.com/dasyx/impersonal_diary",
+      technologies: ["ASTRO", "DAISYUI", "PRISMA"],
+      mustsees: [
+        "Application web de journal intime",
+        "Utilisation de Prisma pour le CRUD",
+        "Templates DaisyUI",
+      ],
+    },
+  ];
+</script>
+
+<div
+  class="playground-container grid grid-cols-4 md:grid-cols-6 sm:grid-cols-12"
+>
+  <h3 class="mt-6 ml-6">EN COURS</h3>
+  {#each projets as projet, index}
+    <div
+      class="col-span-4 sm:col-span-12 md:col-span-6 lg:pl-6 project-item project-{index}"
+    >
+      <div class="flex flex-col xl:flex-row">
+        <figure
+          class="group relative overflow-hidden blur-on-hover w-full h-64 lg:w-1/2"
+        >
+          <img
+            src={projet.imgSrc}
+            alt={projet.altText}
+            class="w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-110"
+          />
+          <div
+            class="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out flex flex-col w-full h-full"
+          >
+            <div
+              class="flex-1 bg-gray-800 bg-opacity-60 flex items-center justify-center"
+            >
+              <a
+                href={projet.projectUrl}
+                target="_blank"
+                rel="noopener"
+                class="text-white">Voir le site</a
+              >
+            </div>
+            <div
+              class="flex-1 bg-gray-500 bg-opacity-60 flex items-center justify-center"
+            >
+              <a
+                href={projet.codeUrl}
+                target="_blank"
+                rel="noopener"
+                class="text-white">Voir le code</a
+              >
+            </div>
+          </div>
+        </figure>
+        <div class="descriptif-projet mt-2 md:mt-0 md:ml-4 w-full">
+          {#each projet.technologies as tech}
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >{tech}</span
+            >
+          {/each}
+          <div class="label-et-listes">
+            <ul class="listes-a-puces">
+              {#each projet.mustsees as mustsee}
+                <li>{mustsee}</li>
+              {/each}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  {/each}
+</div>
+
+<style>
+  figure {
+    transition: all 0.5s ease;
+    z-index: 10;
+    -webkit-box-shadow: 0px 0px 55px 23px rgba(46, 116, 255, 0.9);
+    -moz-box-shadow: 0px 0px 55px 23px rgba(46, 116, 255, 0.9);
+    box-shadow: 0px 0px 55px 23px rgba(46, 116, 255, 0.9);
+    margin-right: 30px;
+  }
+  figure img {
+    border: blanchedalmond 1px solid;
+    border-radius: 5px;
+  }
+  .blur-on-hover {
+    transition: filter 0.5s ease;
+  }
+  .blur-on-hover:hover img {
+    filter: blur(5px); /* Ajustez la valeur du flou selon vos préférences */
+  }
+  .playground-container {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .playground-container h3 {
+    text-align: left;
+  }
+  .playground-container figure {
+    margin-bottom: 1rem;
+  }
+  .project-0 {
+    margin-top: 6rem;
+  }
+  .project-1,
+  .project-2,
+  .project-3 {
+    margin-top: 3rem;
+  }
+  .project-item:last-child {
+    margin-bottom: 0;
+  }
+  .label-et-listes {
+    border-top: 1px solid #ccc;
+    padding-top: 10px;
+  }
+
+  .label-et-listes .label {
+    display: block;
+    text-align: center;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+
+  .label-et-listes .listes-a-puces {
+    list-style-type: disc;
+    padding-left: 20px;
+  }
+</style>
